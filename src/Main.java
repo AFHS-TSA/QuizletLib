c12992f7c2766d34fc4e6b9745c2944fc924d675
JasonLong24 <jasonlongball@gmail.com>
2018-06-07 18:16:20 -0400

    Remade it with a json parser

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,17 @@
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gson.Gson;
 
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-        URL base = new URL("https://api.quizlet.com/2.0/sets/234104177/?client_id=UNNa57NRpT");
+	public static void main(String[] args)  {
 
-		InputStreamReader baseReader = new InputStreamReader(base.openStream());
-        BaseDto baseDto = new Gson().fromJson(baseReader, BaseDto.class);
-
-        System.out.println(baseDto.title);
+	}
         
-        System.out.println(baseDto.terms);
-        
-        String testing[] = baseDto.terms.toString().split("term=");
-        for(int i=1;i<testing.length;i++) {
-        	int j = testing[i].indexOf(' ');
-        	System.out.println(testing[i].substring(0, j));
-        }
-    }
-	
-    private static class BaseDto<Item> {
-    	List<Item> terms;
-    	String term;
-    	String title;
-    	String url;
-        
-    }
 }
 
--- src/Quizlet.java
+++ src/Quizlet.java
@@ -1,33 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Quizlet {
 
 	private static String CLIENT_ID;
 	private static String SET_ID;
 	private static String full;
-	
-	public Quizlet(String clientID, String setID) {
+
+	public static JSONObject setQuiz(String clientID, String setID) throws Exception {
 		CLIENT_ID = clientID;
 		SET_ID = setID;
-		
+
 		String set = "https://api.quizlet.com/2.0/sets/" + SET_ID;
 		String full = set + "?client_id=" + CLIENT_ID;
-		
+
+		URL obj = new URL(full);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		// optional default is GET
+		con.setRequestMethod("GET");
+		// add request header
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		JSONObject myResponse = new JSONObject(response.toString());
+
+		return myResponse;
+		 
+	}	
+
+	public static String[] getTerms(String clientID, String setID) throws JSONException, Exception {
+		JSONArray terms_data = setQuiz(clientID, setID).getJSONArray("terms");
+		String[] terms = new String[terms_data.length()];
+	     for(int i=0;i<terms_data.length();i++) {
+	    	 terms[i] = terms_data.getJSONObject(i).getString("term");
+	     }
+	     return terms;
 	}
 	
-	public String getClientID() {
+	public static String[] getDefinition(String clientID, String setID) throws JSONException, Exception {
+		JSONArray terms_data = setQuiz(clientID, setID).getJSONArray("terms");
+		String[] definition = new String[terms_data.length()];
+	     for(int i=0;i<terms_data.length();i++) {
+	    	 definition[i] = terms_data.getJSONObject(i).getString("definition");
+	     }
+	     return definition;	
+	}
+	
+	public static String getClientID() {
 		return CLIENT_ID;
 	}
-	
-	public String getSetID() {
+
+	public static String getSetID() {
 		return SET_ID;
 	}
-	
-	public String getPlain() {
+
+	public static String getPlain() {
 		return full;
 	}
 
